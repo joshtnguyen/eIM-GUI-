4e9b4ba268414c3aaca85ff7564f155f65de363a
joshtnguyen <55468426+joshtnguyen@users.noreply.github.com>
2022-05-18 09:32:18 -0700

    Formatted Code

--- BackupManager.java
+++ BackupManager.java
@@ -8,66 +8,69 @@ import java.time.LocalDateTime;
  * 
  */
 public class BackupManager {
-  /**
-  * getAllFiles() method to return all files in backup
-  * @return fileList - list of all backup files
-  */
+	/**
+	 * getAllFiles() method to return all files in backup
+	 * 
+	 * @return fileList - list of all backup files
+	 */
 	public static String[] getAllFiles() {
 		File file = new File(FileManager.BACKUP_DIRECTORY);
 		String[] fileList = file.list();
-	    return fileList;
+		return fileList;
 	}
-  
+
 	/**
-  * printViewableFiles() method to return all files in backup
-  * @return fileList - list of all backup files
-  */
+	 * printViewableFiles() method to return all files in backup
+	 * 
+	 * @return fileList - list of all backup files
+	 */
 	public static void printViewableFiles() {
 		String[] fileList = getAllFiles();
 		if (fileList.length > 0) {
 			System.out.println("List of Backups (" + fileList.length + ")\n");
 			for (int i = 0; i < fileList.length; i++) {
-				System.out.println("  " + (i + 1) + ") " + fileList[i]); 
+				System.out.println("  " + (i + 1) + ") " + fileList[i]);
 			}
 		} else {
 			System.out.println("List of Backups (0)\n\n  No backup files here!");
 		}
 	}
-	
+
 	public static String saveBackup() throws IOException {
 		try {
 			String time = "" + LocalDateTime.now();
 			time = time.replace(":", "--");
-			
+
 			FileManager.pushInventory(FileManager.BACKUP_DIRECTORY + time);
 			System.out.println("Backup <<" + FileManager.BACKUP_DIRECTORY + time + ".txt>> has been created.");
-      return time;
+			return time;
 		} catch (Exception e) {
 			System.out.println("Error on Backup");
-      return null;
+			return null;
 		}
 	}
 
-  public static String deleteBackup(String fileName) throws IOException {
-    try {
-      File file = new File(FileManager.BACKUP_DIRECTORY + fileName);
-      if (file.delete()) {
-        return fileName;
-      } else {
-        return null;
-      }
-    } catch (Exception e) {
-      System.out.println("Error on Backup Removal");
-      return null;
-    }
-  }
-  
-/**
-  *getFileName for the Item class
-  *@param name - name of the item 
-  *@param type - type of the item
-  *@param description - description of the condition of the item
-  */	
+	public static String deleteBackup(String fileName) throws IOException {
+		try {
+			File file = new File(FileManager.BACKUP_DIRECTORY + fileName);
+			if (file.delete()) {
+				return fileName;
+			} else {
+				return null;
+			}
+		} catch (Exception e) {
+			System.out.println("Error on Backup Removal");
+			return null;
+		}
+	}
+
+	/**
+	 * getFileName for the Item class
+	 * 
+	 * @param name        - name of the item
+	 * @param type        - type of the item
+	 * @param description - description of the condition of the item
+	 */
 	public static String getFileName(int backupFile) {
 		String[] fileList = getAllFiles();
 		if (backupFile <= 0) {
@@ -78,11 +81,11 @@ public class BackupManager {
 		return fileList[backupFile - 1];
 	}
 
-
-/**
-  *getFileLength() to return the length of fileList
-  *@return int - length of fileList or 0 if null
-  */
+	/**
+	 * getFileLength() to return the length of fileList
+	 * 
+	 * @return int - length of fileList or 0 if null
+	 */
 	public static int getFileLength() {
 		String[] fileList = getAllFiles();
 		if (fileList == null) {
--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 import java.util.*;
 import java.io.*;
 
-
 /**
  * 
  * @author Josh Nguyen
@@ -12,63 +11,63 @@ public class Main {
 
 	public static void main(String[] args) throws IOException, FileNotFoundException {
 
-    FileManager.pullInventory();
-    FileManager.pullUsers();
-    System.out.println(FileManager.getUsers());
-    //adminLogin();
-    GUIManager loginWindow = new GUIManager();
-    loginWindow.openWindow("Login");
-    loginWindow.showLoginWindow();
-    
-  }
-
-  public static String checkLogin(String username, String password) throws IOException, FileNotFoundException {
-    String login = "N/A";
-
-    // Admin Logins
-    File file = new File(FileManager.USER_DIRECTORY + "admin.txt");
-    Scanner reader = new Scanner(file);
-    while(reader.hasNextLine()) {
-      if (reader.nextLine().indexOf("<U: " + username + ", P: " + password + ">") != -1) {
-        login = "ADMIN";
-      }
-    }
-    reader.close();
-    
-    // User Logins
-    File f = new File(FileManager.USER_DIRECTORY + "user.txt");
-    Scanner r = new Scanner(f);
-    r.nextLine();
-    r.nextLine();
-    while (r.hasNextLine()) {
-      String l1 = r.nextLine();
-      String l2 = r.nextLine();
-      String l3 = r.nextLine();
-      String l4 = r.nextLine();
-      if ((l2.equals(username)) && (l3.equals(password))) {
-        login = "USER";
-      }
-    }
-    return login;
-  }
-
-  /**
-  * Developer Testing System for Admin Panel
-  */
-  public static void adminLogin() throws IOException {
-		
-    Scanner sc = new Scanner(System.in);
-		
 		FileManager.pullInventory();
-		
+		FileManager.pullUsers();
+		System.out.println(FileManager.getUsers());
+		// adminLogin();
+		GUIManager loginWindow = new GUIManager();
+		loginWindow.openWindow("Login");
+		loginWindow.showLoginWindow();
+
+	}
+
+	public static String checkLogin(String username, String password) throws IOException, FileNotFoundException {
+		String login = "N/A";
+
+		// Admin Logins
+		File file = new File(FileManager.USER_DIRECTORY + "admin.txt");
+		Scanner reader = new Scanner(file);
+		while (reader.hasNextLine()) {
+			if (reader.nextLine().indexOf("<U: " + username + ", P: " + password + ">") != -1) {
+				login = "ADMIN";
+			}
+		}
+		reader.close();
+
+		// User Logins
+		File f = new File(FileManager.USER_DIRECTORY + "user.txt");
+		Scanner r = new Scanner(f);
+		r.nextLine();
+		r.nextLine();
+		while (r.hasNextLine()) {
+			String l1 = r.nextLine();
+			String l2 = r.nextLine();
+			String l3 = r.nextLine();
+			String l4 = r.nextLine();
+			if ((l2.equals(username)) && (l3.equals(password))) {
+				login = "USER";
+			}
+		}
+		return login;
+	}
+
+	/**
+	 * Developer Testing System for Admin Panel
+	 */
+	public static void adminLogin() throws IOException {
+
+		Scanner sc = new Scanner(System.in);
+
+		FileManager.pullInventory();
+
 		System.out.println("\nWelcome to Rental Inventory Manager! (ADMIN PANEL)\n");
 		printCommands();
-		
+
 		String input = "";
 		do {
 			System.out.print("\nCommand >> ");
 			input = feedback(sc).toLowerCase();
-			
+
 			boolean failCommand = true;
 			if (input.equals("/list")) {
 				System.out.println("List of Items (" + FileManager.getInventorySize() + ") \n" + FileManager.getList());
@@ -77,10 +76,10 @@ public class Main {
 			if (input.equals("/additem")) {
 				failCommand = false;
 				FileManager.addItem();
-      }
-      if (input.equals("/removeitem")) {
-        failCommand = false;
-        FileManager.removeItem();
+			}
+			if (input.equals("/removeitem")) {
+				failCommand = false;
+				FileManager.removeItem();
 			}
 			if (input.equals("/savebackup")) {
 				BackupManager.saveBackup();
@@ -117,27 +116,27 @@ public class Main {
 				System.out.println("Invalid Command! Try one of the few below: \n");
 				printCommands();
 			}
-			
+
 		} while (!input.equals("/quit"));
-		
+
 		FileManager.pushInventory();
 		System.out.println("Have a great day!");
 	}
-	
+
 	public static void printCommands() {
 		System.out.println("Commands:");
 		System.out.println(" /list");
 		System.out.println(" /addItem");
-    System.out.println(" /removeItem");
+		System.out.println(" /removeItem");
 		System.out.println(" /saveBackup");
 		System.out.println(" /loadBackup");
 		System.out.println(" /listBackups");
 		System.out.println(" /quit");
 	}
-	
+
 	public static String feedback(Scanner sc) {
 		String input = sc.nextLine();
 		return input;
 	}
-	
+
 }
